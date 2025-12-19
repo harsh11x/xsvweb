604142e7dcb31d19c918de4dacc4b0aea7ec1f45
Harsh Dev <harshdev@Mac.lan>
2025-12-20 00:17:55 +0530

    Update home page background shader colors to Navy Blue/Off-White

--- app/page.tsx
+++ app/page.tsx
@@ -59,8 +59,8 @@ export default function Home() {
       >
         <Shader className="h-full w-full">
           <Swirl
-            colorA="#1275d8"
-            colorB="#e19136"
+            colorA="#001F3F"
+            colorB="#FAF9F6"
             speed={0.8}
             detail={0.8}
             blend={50}
@@ -72,11 +72,11 @@ export default function Home() {
             fineY={40}
           />
           <ChromaFlow
-            baseColor="#0066ff"
-            upColor="#0066ff"
+            baseColor="#001F3F"
+            upColor="#001F3F"
             downColor="#d1d1d1"
-            leftColor="#e19136"
-            rightColor="#e19136"
+            leftColor="#FAF9F6"
+            rightColor="#FAF9F6"
             intensity={0.9}
             radius={1.8}
             momentum={25}
